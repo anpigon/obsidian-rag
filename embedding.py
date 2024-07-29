1321ff385e52ed49382e69735de0748242f34f1e
anpigon <markan82@gmail.com>
2024-07-29 14:37:03 +0900

    feat(embedding.py): Obsidian 노트를 처리하는 새로운 스크립트 추가 Obsidian 노트를 로드하고 임베딩을 생성하는 기능을 구현하기 위해 새로운 스크립트를 추가하였다.

---
+++ embedding.py
@@ -0,0 +1,75 @@
+# %%
+from dotenv import load_dotenv
+
+load_dotenv()
+
+import argparse
+from pathlib import Path
+
+import streamlit as st
+from langchain.embeddings import CacheBackedEmbeddings
+from langchain.storage import LocalFileStore
+from langchain.text_splitter import Language, RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+
+from document_loaders.obsidian import MyObsidianLoader
+
+# %%
+root_path = Path.cwd()
+
+# %%
+store = LocalFileStore(root_path / ".cached_embeddings")
+
+# %%
+model_name = "intfloat/multilingual-e5-large-instruct"
+model_kwargs = {"device": "mps"}  # Change to "cuda" if using GPU
+encode_kwargs = {"normalize_embeddings": False}
+
+underlying_embeddings = HuggingFaceEmbeddings(
+    model_name=model_name,
+    model_kwargs=model_kwargs,
+    encode_kwargs=encode_kwargs,
+)
+
+# %%
+namespace = getattr(
+    underlying_embeddings,
+    "model",
+    getattr(underlying_embeddings, "model_name", model_name),
+)
+
+
+# %%
+cached_embeddings = CacheBackedEmbeddings.from_bytes_store(
+    underlying_embeddings=underlying_embeddings,
+    document_embedding_cache=store,
+    namespace=getattr(
+        underlying_embeddings,
+        "model",
+        getattr(underlying_embeddings, "model_name", model_name),
+    ),
+)
+
+
+def main(obsidian_path: str):
+    loader = MyObsidianLoader(obsidian_path, encoding="utf-8", collect_metadata=True)
+
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter.from_language(
+        chunk_size=1024, chunk_overlap=24, language=Language.MARKDOWN
+    )
+    texts = text_splitter.split_documents(documents)
+
+    vectorstore = Chroma.from_documents(
+        texts,
+        cached_embeddings,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process Obsidian Notes.")
+    parser.add_argument("obsidian_path", type=str, help="Path to the Obsidian note")
+    args = parser.parse_args()
+
+    main(args.obsidian_path)
